51b94801d77563599486845c731e0b64cb7bfc88
s0uvik <souviksarkar260123@gmail.com>
2022-10-06 19:17:20 +0530

    message

---
+++ main.js
@@ -0,0 +1,52 @@
+fetch(
+  "https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature"
+)
+  .then((res) => res.json())
+  .then((data) => {
+    document.body.style.backgroundImage = `url(${data.urls.regular})`;
+    document.getElementById("author").textContent = `By: ${data.user.name}`;
+  })
+  .catch((err) => {
+    document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDE2NzA&ixlib=rb-1.2.1&q=80&w=1080)`;
+  });
+
+fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
+  .then((res) => {
+    if (!res.ok) {
+      throw Error("Something went wrong");
+    }
+    console.log(res.status);
+    return res.json();
+  })
+  .then((data) => {
+    document.querySelector(
+      ".crypto"
+    ).textContent = `Dogecoin: ₹${data.market_data.current_price.inr}`;
+  })
+  .catch((err) => console.error(err));
+
+function getCurrentTime() {
+  const date = new Date();
+  document.querySelector(".time").textContent = date.toLocaleTimeString({
+    timeStyle: "medium",
+  });
+}
+setInterval(getCurrentTime, 1000);
+
+navigator.geolocation.getCurrentPosition((position) => {
+  fetch(
+    `https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=imperial`
+  )
+    .then((res) => {
+      if (!res.ok) {
+        throw Error("Weather data not available");
+      }
+      return res.json();
+    })
+    .then((data) => {
+      document.querySelector(".weather").textContent = `${Math.round(
+        (data.main.temp - 32) / 1.8
+      )}°C`;
+    })
+    .catch((err) => console.error(err));
+});
